2fe8a5e176c50091cefafdc2d1426c3e2d450d6e
thanthansint <masintlay@gmail.com>
2020-03-08 12:33:06 -0600

    Added 1 method of change to upper case

--- object.js
+++ object.js
@@ -2,15 +2,18 @@ class kayneQuote{
     constructor(quote){
         this.kayneWisdom = quote;
     }
-    changeCase1() {
-
+    changeUpperCase() {
+        this.newkayneWisdom = this.kayneWisdom.toUpperCase();
+        return this.newkayneWisdom;
     }
-    changeCase2() {
-        
-    }
-    changeCase3() {
-        
-    }
+    // changeLowerCase() {
+    //     this.newkayneWisdom = this.kayneWisdom.toLowerCase();
+    //     return this.newkayneWisdom;
+    // }
+    // splitCase() {
+    //     this.newkayneWisdom = this.kayneWisdom.split(" ");
+    //     return this.newkayneWisdom;
+    // }
 }
 
 //make AJAX request to get quote
@@ -20,8 +23,18 @@ class kayneQuote{
 let ajax = new XMLHttpRequest();
 ajax.onreadystatechange = function(){
     if (ajax.status == 200 && ajax.readyState == 4) {
-        document.getElementById('display').innerText = ajax.responseText;
+        var quoteJSON = JSON.parse(ajax.responseText);
+        document.getElementById('display').innerText = quoteJSON.quote;
+        console.log(quoteJSON.quote);
+        let obj = new kayneQuote(quoteJSON.quote);
+        console.log(obj.changeUpperCase());
+        // console.log(obj.changeLowerCase());
+        // for (i=0; i<obj.splitCase().length; i++){
+        //     console.log(obj.splitCase()[i]);
+        // }       
     }
-    ajax.open('GET', 'https://api.kanye.rest/', true);
-    ajax.send();
 }
+ajax.open('GET', 'https://api.kanye.rest/', true);
+ajax.send();
+
+
